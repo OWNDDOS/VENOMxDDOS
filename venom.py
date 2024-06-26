25f5c041a405ca3d5b23e26cbc6b84e238e6e0b7
OWNDDOS <Piyushsahu16065@gmail.com>
2024-06-26 16:06:37 +0530

    Update venom.py

--- venom.py
+++ venom.py
@@ -4,8 +4,8 @@ import logging
 from aiogram import Bot
 import asyncio
 
-API_TOKEN = 'TOKEN'
-ADMIN_ID = 'Your Id'
+API_TOKEN = '7479567686:AAEd6nTNl70P9wEnnDLrVB9nMAzFxHnmsVQ'
+ADMIN_ID = '6243315730'
 MAX_RESTARTS = 5
 RESTART_PERIOD = 60  # Seconds
 
